019e3641cb08f5c17197b43f48d27f328cd2f3ef
Prabin Rag TK <prabin.rag@claysys.in>
2024-06-10 21:41:44 +0530

    Test Success

---
+++ bin/myfirstproject.dart
@@ -0,0 +1,57 @@
+// void main() {
+//   var a = stdin.readLineSync();
+//   print('Hello $a');
+// }
+
+// void main() {
+//   print('Enter Two numbers\n');
+//   var num1 = stdin.readLineSync();
+//   var num2 = stdin.readLineSync();
+//   var sum = int.parse(num1!) + int.parse(num2!);
+//   print('The Sum of two numbers is $sum');
+//   print(num1.length);
+// }
+
+// void main() {
+//   print('enter a number');
+//   var number = int.parse(stdin.readLineSync()!);
+//   if (number == 0) {
+//     print('You entered zero');
+//   } else if (number > 0) {
+//     print('Entered a value grater than zero');
+//   } else {
+//     print('Entered a value less than zero');
+//   }
+// }
+
+// void main() {
+//   List<int> num = [1, 2, 5, 3, 5, 1, 5];
+//   print(num);
+//   if (num.contains(5)) num.remove(5);
+//   print(num);
+//   if (num.contains(5)) num.remove(5);
+//   print(num);
+//   if (num.contains(5)) num.remove(5);
+//   print(num);
+// }
+
+// void main() {
+//   List<List<int>> numbers = [
+//     [1, 2, 5, 4],
+//     [8, 6, 5, 2]
+//   ];
+//   print(numbers[1][1]);
+// }
+
+// void main() {
+//   List<int> list = [1, 5, 8, 25, 8, 842, 6];
+//   Set<int> set = {1, 5, 8, 25, 8, 842, 6};
+//   print(list);
+//   print(set);
+// }
+
+// void main() {
+//   Map<int, String> maps = {1: 'prabin', 2: 'aswani'};
+//   print(maps[2]);
+// }
+
